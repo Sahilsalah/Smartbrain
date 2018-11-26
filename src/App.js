841bee447f75393eb935330ba623c8c014328097
Sahilsalah <42202259+Sahilsalah@users.noreply.github.com>
2018-11-26 11:01:54 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import 'tachyons'
+import Clarifai from 'clarifai'
+import Particles from 'react-particles-js';
 
+const app = new Clarifai.App({
+ apiKey: '9f2f849619254403bfc8bce1a9e259f8'
+});
+
+
+const particlesOptions={
+  
+    particles: {
+      number:{
+        Value:30,
+        density:{
+          enable:true,
+          value_area:800
+        
+               }
+           }
+        }
+}
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:'',
+      imageUrl:'',
+      box:{},
+    }
+  }
+
+  calculateFace=(data)=>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height= Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col *width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col*width),
+      bottomRow: height - (clarifaiFace.bottom_row *height)
+    }
+  }
+
+displayFaces=(box)=>{
+  console.log(box);
+  this.setState({box:box});
+}
+
+
+  onInputChange=(event)=> {
+    this.setState({input:event.target.value});
+  }
+
+  onSubmit=()=>{
+    this.setState({imageUrl:this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
+    .then(response =>this.displayFaces(this.calculateFace(response)))
+    .catch(err=>console.log(err));
+    
+      
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className='particles'
+          params={particlesOptions}/>
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+
+      
+    
       </div>
     );
   }
